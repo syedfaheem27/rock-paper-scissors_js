7172cc03e746bf5539fa2c2af985c1581e2cedfa
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-20 14:57:37 +0530

    Modify the playRound function print the player and computer choice before declaring the result in the developer's console

--- javascript/script.js
+++ javascript/script.js
@@ -22,30 +22,48 @@ function getUserChoice() {
 }
 function playRound(playerSelection,computerSelection){
     if(computerSelection=="ROCK"&&playerSelection=="ROCK"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("It's a tie as both selections are ROCK");
     }
     else if (computerSelection=="ROCK"&&playerSelection=="SCISSOR"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("Computer wins. Rock breaks SCISSOR");
     }
     else if(computerSelection=="ROCK"&&playerSelection=="PAPER"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("You win. PAPER wraps ROCK");
     }
     else if(computerSelection=="PAPER"&&playerSelection=="ROCK"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("Computer wins. PAPER wraps ROCK");
     }
     else if(computerSelection=="PAPER"&&playerSelection=="PAPER"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("It's a tie as both selections are PAPER");
     }
     else if (computerSelection=="PAPER"&&playerSelection=="SCISSOR"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("You win. SCISSOR cuts PAPER");
     }
     else if(computerSelection=="SCISSOR"&&playerSelection=="ROCK"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("You win. ROCK breaks SCISSOR");
     }
     else if(computerSelection=="SCISSOR"&&playerSelection=="PAPER"){
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("Computer wins. SCISSOR cuts PAPER");
     }
     else {
+        console.log("Player's choice = " + playerSelection);
+        console.log("Computer's choice = " + computerSelection);
         console.log("It's a tie as both slections are SCISSOR");
     }
 }

d86038e42ca355bbdd43bb426cc5c422c1cdfffe
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-21 11:38:50 +0530

    Edit the range of the random function Scaled the random function such that it gives three distinct values.

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,9 @@
 function getComputerChoice() {
-    let num=Math.floor(Math.random()*100);
-    if(num==99){
-        for(let i=0;num==99;i++){
-            num=Math.floor(Math.random()*100);
-        }
-    }
-    if(num>=0&&num<33){
+    let num=Math.floor(Math.random()*3);
+    if(num==0){
         return "ROCK";
     }
-    else if(num>=33&&num<66){
+    else if(num==1){
         return "PAPER";
     }
     else {

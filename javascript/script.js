c9c540efffd9a573b4c21341c681ddb3a647ebbd
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-03 20:23:34 +0530

    Add the rock,paper and scissor button to the DOM and add some css to them.

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,24 @@
+function Rock_Paper_Scissor() {
+    const div_game=document.createElement('div');
+    div_game.classList.add('game');
+    const btn_rock=document.createElement('button');
+    const btn_paper=document.createElement('button');
+    const btn_scissor=document.createElement('button');
+    document.querySelector('body')
+    .appendChild(div_game);
+    btn_rock.textContent="Rock";
+    btn_paper.textContent="Paper";
+    btn_scissor.textContent="Scissor";
+    div_game.appendChild(btn_rock);
+    div_game.appendChild(btn_paper);
+    div_game.appendChild(btn_scissor);
+
+}
 const btn=document.querySelector('button');
 console.log(btn);
 btn.addEventListener('click', (e) => {
     console.log(e.target);
     btn.classList.add('click');
+    Rock_Paper_Scissor();
+    
 })

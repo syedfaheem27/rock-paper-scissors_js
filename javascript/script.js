f46544a0f836f08c199a5b3b627729eb1a3245d7
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-04 19:48:32 +0530

    Add event listener and remove the background image- Added an event listener for the three buttons and add a function which declares the result of each round. Also, remove the background image and add a background.

--- javascript/script.js
+++ javascript/script.js
@@ -4,25 +4,89 @@ const btn_rock=document.createElement('button');
 const btn_paper=document.createElement('button');
 const btn_scissor=document.createElement('button');
 let score_user=0,score_comp=0;
+const result=document.createElement('div');
+const score=document.createElement('div');
+
 
 btn.addEventListener('click', (e) => {
-    console.log(e.target);
     btn.classList.add('click');
     Rock_Paper_Scissor();
     function Rock_Paper_Scissor() {
-        div_game.classList.add('game');
+        div_game.classList.add('box');
         document.querySelector('body')
         .appendChild(div_game);
         btn_rock.textContent="Rock";
         btn_paper.textContent="Paper";
         btn_scissor.textContent="Scissor";
-        btn_rock.setAttribute('data-id','1');
-        btn_paper.setAttribute('data-id','2');
-        btn_scissor.setAttribute('data-id','3');
+        btn_rock.dataset.id='0';
+        btn_paper.dataset.id='1';
+        btn_scissor.dataset.id='2';
         div_game.appendChild(btn_rock);
         div_game.appendChild(btn_paper);
-        div_game.appendChild(btn_scissor);
+        div_game.appendChild(btn_scissor); 
         }
+    });   
+    div_game.addEventListener('click',(e) => {
+        if(!e.target.dataset.id) return;
+        let user_choice=e.target.dataset.id,
+        comp_choice=Math.floor(Math.random()*3);//0-rock,1-paper,2-scissor
+        playRound(user_choice,comp_choice);
     });
+    function playRound(u,c) {
+        result.textContent=" ";
+        score.textContent=" ";
+        document.querySelector('body')
+        .appendChild(result);
+        document.querySelector('body')
+        .appendChild(score);       
+        if(u==0){
+            if(c==0){
+                result.textContent="Computer chooses rock and it's a tie.";
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;
+            }
+            else if(c==1){                
+                result.textContent="Computer chooses paper and wins.";
+                score_comp+=1;
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;              
+            }
+            else {
+                result.textContent="Computer chooses scissor and loses.";
+                score_user+=1;
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;
+            }
+        }
+        else if(u==1){
+            if(c==0){
+                score_user+=1;
+                result.textContent="Computer chooses rock and loses.";
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;
+            }
+            else if(c==1){                
+                result.textContent="Computer chooses paper and it's a tie.";
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;              
+            }
+            else {
+                result.textContent="Computer chooses scissor and wins.";
+                score_comp+=1;
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;
+            }
 
+        }
+        else {
+            if(c==0){
+                result.textContent="Computer chooses rock and wins.";
+                score_comp+=1;
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;
+            }
+            else if(c==1){                
+                result.textContent="Computer chooses paper and loses.";
+                score_user+=1;
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;              
+            }
+            else {
+                result.textContent="Computer chooses scissor and it's a tie.";
+                score.textContent="User_score="+score_user + " and Computer_score="+score_comp;
+            }
+        }
+    }  
     
